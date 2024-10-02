2d6aafb2fcf297e3d49630227632ab820a8c74d2
gabsxiz7 <gabriellalacerda10@gmail.com>
2024-10-02 15:34:02 -0300

    Alterado

---
+++ php/Insert.php
@@ -0,0 +1,34 @@
+<?php
+
+//Conectando no Banco de Dados
+
+include 'conexao.php';
+
+//Receber os Dados do Formulário
+
+$nome = $_POST['nome'];
+$email = $_POST ['email'];
+$cargo = $_POST ['cargo'];
+$senha = $_POST ['senha'];
+
+$sql = "INSERT INTO tb_user VALUES (null, '$nome', '$email', '$cargo', '$senha')";
+
+// Executar o Insert no Banco de Dados
+
+$conexao -> query($sql);
+
+if ($conexao -> query($sql)){
+    echo "<script> alert('✔ Inserido com Sucesso!'); history.back(); <script>";
+}
+
+else{
+    echo "Falha na Conexão com o Banco de Dados";
+}
+
+
+
+
+
+
+
+?>
--- php/conexao.php
+++ php/conexao.php
@@ -1,25 +1,20 @@
-<?php 
-//Informações para conectar no banco de dados
+<?php
+
+// Conectando no Banco de Dados
 
 $servidor = 'localhost';
 $usuario = 'root';
 $senha = 'root';
 $db = 'db_contabilidade';
 
-$conexao = new mysqli($servidor,$usuario,$senha,$db);
+$conexao = new mysqli($servidor, $usuario, $senha, $db);
 
-if ($conexao->connect_error) {
-    die('Falha na conexão'. $conexao->connect_error);
-}else{
-    echo "Conectado com Sucesso";
+if ($conexao ->connect_error){
+    die('falha na conexão'. $conexao -> connect_error);
 }
-
-
-
-
-
-
-
+else{
+    echo "✔ Conectado com Sucesso!";
+}
 
 
 
